11a986f8238de6d3d188078eb264e6fc7bb9624c
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-26 16:12:31 +0630

    Add default route and server port

---
+++ src/main/java/com/vinn/personalBlog/view/HomeController.java
@@ -0,0 +1,13 @@
+package com.vinn.personalBlog.view;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Personal Blog API!";
+    }
+}
